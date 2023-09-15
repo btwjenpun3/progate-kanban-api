2ae87daa5e06317f2b671c6830c54e58eaf6252f
Helmi <muhamadkelmi@gmail.com>
2023-09-15 21:36:29 +0700

    coba

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="sidebar-container">
         <a class="sidebar-link" href="{{ route('home') }}">
             <span class="material-icons sidebar-icon">home</span>
-            <p class="sidebar-text">Home</p>
+            <p class="sidebar-text">Rumah</p>
         </a>
         <a class="sidebar-link" href="{{ route('tasks.index') }}">
             <span class="material-icons sidebar-icon">list</span>
